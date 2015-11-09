4c090d66b202cd5afdd23df1805f300a76007f0f
Jess Lee <jessleenyc@gmail.com>
2015-11-09 11:26:32 -0500

    adds eventListener to stop game

--- main.js
+++ main.js
@@ -4,97 +4,92 @@ var green = document.querySelector('#green');
 var yellow = document.querySelector('#yellow');
 var start = document.querySelector('.start-button');
 var board = document.querySelector('.game-container');
-var end = document.querySelector('#overlay');
+var end = document.querySelector('.end');
 var endMessage = document.querySelector('.num-rounds');
 var tryAgain = document.querySelector('.try-again');
 var score = document.querySelector('.score');
-
-
+var home = document.querySelector('.home');
+var main = document.querySelector('.main-container');
+var stopButton = document.querySelector('.stop-button');
 
 var pattern = [];
 var attempt = [];
 var numRounds = 0;
 var clicks = 0;
 var counter = 0; //number of correct guesses
+var num = 0; //counter index for pattern array
 
 var randomColor = function () {
-	var random = Math.floor(Math.random() * 4);	
-		if (random === 0) {
-			red.classList.add('opacity');
-		} else if (random === 1) {
-			blue.classList.add('opacity')
-		} else if (random === 2) {
-			green.classList.add('opacity');
-		} else if (random === 3) {
-			yellow.classList.add('opacity');
-		}
+  var random = Math.floor(Math.random() * 4);	
+  if (random === 0) {
+	  red.classList.add('opacity');
+	} else if (random === 1) {
+		blue.classList.add('opacity');
+	} else if (random === 2) {
+		green.classList.add('opacity');
+	} else if (random === 3) {
+		yellow.classList.add('opacity');
+	}
 	pattern.push(random); //stores random # into pattern sequence
 	setTimeout(removeColor, 100); 
-	numRounds++;
 	score.textContent = "ROUND: " + numRounds;
 };
 
 var removeColor = function (event) {
 	if (red.classList.contains('opacity')) {
-			red.classList.remove('opacity')
+			red.classList.remove('opacity');
 		} else if (blue.classList.contains('opacity')) {
-			blue.classList.remove('opacity')
+			blue.classList.remove('opacity');
 		} else if (green.classList.contains('opacity')) {
-			green.classList.remove('opacity')
+			green.classList.remove('opacity');
 		} else if (yellow.classList.contains('opacity')) {
 			yellow.classList.remove('opacity');
 		}
 };
-
 //store user attempt
-	var elementClicked = function (event) { 
-		var element = event.target;	
-		if (element === red) {
-			attempt.push(0); 
-		} else if (element === blue) {
-			attempt.push(1);
-		} else if (element === green) {
-			attempt.push(2);
-		} else if (element === yellow) {
-			attempt.push(3);
-		}
-		clicks++;
-		console.log('click ' + clicks)
-	};
-
-var num = 0; 
+var elementClicked = function (event) { 
+	var element = event.target;	
+	if (element === red) {
+		attempt.push(0); 
+	} else if (element === blue) {
+		attempt.push(1);
+	} else if (element === green) {
+		attempt.push(2);
+	} else if (element === yellow) {
+		attempt.push(3);
+	}
+	clicks++;
+};
 
 var matchColor = function() {
 	if (pattern[num] === 0) {
 		red.classList.add('opacity');
 	} else if (pattern[num] === 1) {
-		blue.classList.add('opacity')
+		blue.classList.add('opacity');
 	} else if (pattern[num] === 2) {
 		green.classList.add('opacity');
 	} else if (pattern[num] === 3) {
 		yellow.classList.add('opacity');
 	}
 	setTimeout(removeColor, 100); 	
-}
+};
 
-var match = function () {
+var match = function () { //plays back previous pattern
 	matchColor();
 	if (num < pattern.length) {
-		next();
+		time();
 	}
 	num++;
-	console.log(num + 'num');
-}
+};
 
 var time = function () {
 	setTimeout(match, 500);
 }
 
-var next = function () {
-	time(); 
+var stop = function () {
+	endMessage.textContent = 'You got through ' + numRounds + ' round(s)	!';
+	end.style.visibility = 'visible';		
 }
-
-
 //check if user matched computer pattern
 var numClicks = function () {
 	if (clicks === pattern.length) {
@@ -105,39 +100,44 @@ var numClicks = function () {
 				}
 		}
 
-	var confirm = function () {
+		var confirm = function () {
 			if (counter === pattern.length) { //complete round, go to next round
-				console.log('pass to next round'); 
-				setTimeout(match, 500);
+				numRounds ++;
+				counter = 0; 
 				attempt = []; //clear attempts for next round
-				counter = 0;
-				var wait = (numRounds + 1) * 500;
+				setTimeout(match, 500); //plays previous pattern
+				var wait = (numRounds + 1) * 500; //waits until pattern is over
 				setTimeout(randomColor, wait); //adds a new random color to pattern
 			} else { //fail round, end game
-				console.log('fail');
-				// endMessage.textContent = 'You got through ' + numRounds + ' round(s)	!';
-				// end.style.visibility = 'visible';	
+					stop();
+				}
 			}
-		}
-		confirm();
-	}
+			confirm();
+		};
 };
 
+var homePage = function () {
+	end.style.visibility = 'hidden';
+	pattern = [];
+	attempt = [];
+	numRounds = 0;
+	clicks = 0;
+	counter = 0;
+	score.textContent = "ROUND: " + numRounds;
+};
 
-
-// var homePage = function () {
-// 	end.style.visibility = 'hidden';
-// 	pattern = [];
-// 	attempt = [];
-// 	numRounds = 0;
-// 	clicks = 0;
-// 	counter = 0;
-// }
+var removeWelcome = function () {
+	home.style.visibility = 'hidden';
+};
 
 board.addEventListener('click', elementClicked);
 board.addEventListener('click', numClicks);
 start.addEventListener('click', randomColor);
-// tryAgain.addEventListener('click', homePage);
+tryAgain.addEventListener('click', homePage);
+window.addEventListener('click', removeWelcome);
+stopButton.addEventListener('click', stop);
+
+
 
 
 
